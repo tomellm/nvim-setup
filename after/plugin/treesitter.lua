759fa498e4d5dd62e7c69e14e00d9ef1d1c4a938
Thomas Ellmenreich <thomas.ellmenreich@student.kdg.be>
2023-04-05 07:49:06 +0200

    added gitigore

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "help", "rust", "python", "c", "lua", "vim", "help", "query" },
+  --ensure_installed = { "rust", "python", "c", "lua", "vim", "help", "query" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
